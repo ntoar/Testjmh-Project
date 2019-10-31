cb947db551abde789930df5c0e0d11628aedf788
ntoar <noeltomas@gmail.com>
2019-10-31 11:10:03 +0100

    Benchmark testing different list sizes

--- maven-testjmh/src/main/java/com/testjmh/maven_testjmh/MyBenchmark.java
+++ maven-testjmh/src/main/java/com/testjmh/maven_testjmh/MyBenchmark.java
@@ -32,6 +32,7 @@
 package com.testjmh.maven_testjmh;
 
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
@@ -39,36 +40,67 @@ import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import com.doglib.Dog;
 import com.doglib.DogLib;
 
 @State(Scope.Benchmark)
 public class MyBenchmark {
 	
-	private DogLib dl = new DogLib("src\\main\\resources\\dogs.xml");	
+	//private DogLib dl = new DogLib("src\\main\\resources\\dogs.xml");
+	private DogLib dl;
+	
+	@Param({"10","100","1000","10000"})
+	private int LISTSIZE;
+	
+	private String name [] = {"James","Lars","Kirk","Robert"};
+	private String date [] = {"10-01-2010","10-02-2010","10-01-2012","10-02-2012"};
+	private double weight [] = {10,15,20,25};
+	private String breed [] = {"German Shepherd","Siberian Husky","Shiba Inu","Labrador Retriever"};
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    @Warmup(iterations=10)
-    @Measurement(iterations=10)
+    @Warmup(iterations=5)
+    @Measurement(iterations=5)
     public void testMethod(Blackhole bh) {
             	
     	Map<String,Double> m =dl.averageWeightPerBreed();
     	bh.consume(m.size());
     }
     
+    
+    @Setup
+    public void setup() {
+    	this.dl=new DogLib();
+    	createList(dl);
+    }
+    
     public static void main(String[] args) throws RunnerException {
     	Options opt = new OptionsBuilder().include(MyBenchmark.class.getSimpleName()).forks(1).build();
     	new Runner(opt).run();
+    }
+    
+    private void createList(DogLib dl) {    	
+    	for(int i=0; i<LISTSIZE;i++) {
+    		
+    		int randnum1=ThreadLocalRandom.current().nextInt(0,4);
+    		int randnum2=ThreadLocalRandom.current().nextInt(0,4);
+    		
+    		Dog d = new Dog(name[randnum1],date[randnum2],weight[randnum1],breed[randnum2]);
+    		dl.addDog(d);
+    	}
+    	
     }
 
 }
